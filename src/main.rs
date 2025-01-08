a8e14450dc71745703091b62644be5c9d8cad7eb
Joe Endacott <joseph.endacott@gmail.com>
2025-01-08 18:22:43 +0000

    Removed unused code.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 pub mod grid;
-pub mod grid_function;
 pub mod scalar_field;
-// pub mod landau_lifshitz_gilbert_equation;
-pub mod numerical_differentiation;
-pub mod numerical_integration;
-pub mod quadratic_interpolation;
 pub mod vector_field;
+// pub mod landau_lifshitz_gilbert_equation;
 
 fn main() {
     println!("Hello, world!");
