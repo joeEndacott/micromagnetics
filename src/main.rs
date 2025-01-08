205b31cbd4fbd14987e9bb97744719cec6bc7d51
Joe Endacott <joseph.endacott@gmail.com>
2025-01-08 15:37:09 +0000

    Added some more old code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+pub mod grid;
+pub mod grid_function;
+pub mod numerical_differentiation;
+pub mod numerical_integration;
+pub mod quadratic_interpolation;
+
 fn main() {
     println!("Hello, world!");
 }
